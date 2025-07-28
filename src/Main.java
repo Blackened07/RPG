950082d875eb17556764ba341b6402349e10a9f4
Blackened <blackened.07@mail.ru>
2025-07-28 21:59:21 +1000

    TryCommit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        Orc orcWar = new Orc(100, 5);
-        Elf elfDruid = new Elf(80, 6);
+        Orc orcWar = new Orc(120, 5);
+        Elf elfDruid = new Elf(95, 6);
         Fight.Fighting(orcWar, elfDruid);
 
     }
